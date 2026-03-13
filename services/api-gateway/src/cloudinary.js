e1901a11e53d77af08658befb62f53d1a4e5eaa5
Vivekkarakoti <Viveksngh343@gmail.com>
2026-03-13 23:16:42 +0530

    Update Cloudinary configuration for production

--- services/api-gateway/src/cloudinary.js
+++ services/api-gateway/src/cloudinary.js
@@ -1,12 +1,9 @@
 const cloudinary = require('cloudinary').v2;
-const { config } = require('dotenv');
-
-config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'demo',
-  api_key: process.env.CLOUDINARY_API_KEY || 'demo',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'demo'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dt88zbwdt',
+  api_key: process.env.CLOUDINARY_API_KEY || '937254738876683',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'aUWweDSeCwpINyCbGAyR8G8UMC4'
 });
 
 module.exports = cloudinary;
